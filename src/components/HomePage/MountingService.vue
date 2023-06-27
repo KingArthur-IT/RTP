82128f157f5a6504e33a772ea07e2bbdb4d38a11
Artem <OstapT5@gmail.com>
2023-06-27 16:45:35 +0200

    mounting section edits

--- src/components/HomePage/MountingService.vue
+++ src/components/HomePage/MountingService.vue
@@ -4,12 +4,10 @@
           <div class="section-title-text">{{ title }}</div>
       </div>
       <div class="mounting__images">
-          <div v-for="loop in loopsCount" :key="loop">
-              <div class="mounting__img-wrapper" :class="{'six': sectionCount(loop) === 6, 'nine': sectionCount(loop) === 9}">
-                  <div class="mounting__item" v-for="index in sectionCount(loop)" :key="index">
-                      <div class="mounting__img">
-                          <img :src="getImageUrl('mounting', (loop - 1) * 9 + index, 'jpg')" :alt="index">
-                      </div>
+          <div v-for="row in curRows" :key="row" class="mounting__img-wrapper" :class="{'long': row === 1 || row === 4}">
+              <div v-for="i in 3" :key="i" class="mounting__item">
+                  <div class="mounting__img">
+                      <img :src="getImageUrl('mounting', i + (row - 1) * 3, 'jpg')" :alt="i">
                   </div>
               </div>
           </div>
@@ -30,8 +28,8 @@ export default {
     },
     data() {
         return {
-            imageCount: 6,
-            maxImageCount: 15
+            curImagesCount: 6,
+            maxImageCount: 12
         }
     },
     props: {
@@ -41,27 +39,17 @@ export default {
         }
     },
     computed: {
-        loopsCount() {
-            return Math.floor(this.imageCount / 10) + 1
+        curRows() {
+            return Math.round(this.curImagesCount / 3)
         },
         hasMoreImages() {
-            return this.imageCount < this.maxImageCount
+            return this.curImagesCount < this.maxImageCount
         }
     },
     methods: {
         getImageUrl,
-        sectionCount(loop) {
-            if (loop < this.loopsCount)
-                return 9
-            else {
-                const remainder = this.imageCount % 9
-                return remainder === 0 ? 9 : 6
-            }
-        },
         showMore() {
-            if (this.imageCount % 9 === 6)
-                this.imageCount += 3
-            else this.imageCount += 6
+            this.curImagesCount += 3
         }
     }
 }
@@ -74,51 +62,26 @@ export default {
         width: calc( 100% + 48px )
         transform: translateX(-24px)
     &__img-wrapper
-        display: grid
-        grid-column-gap: 0px
-        grid-row-gap: 0px
-        &.six
-            grid-template-columns: repeat(3, 1fr)
-            grid-template-rows: repeat(5, auto)
+        display: flex
+        align-items: stretch
+        &.long
             & .mounting__item
                 &:nth-child(1)
-                    grid-area: 1 / 1 / 3 / 2
+                    flex-basis: 26%
                 &:nth-child(2)
-                    grid-area: 3 / 1 / 6 / 2
+                    flex-basis: 48%
                 &:nth-child(3)
-                    grid-area: 1 / 2 / 4 / 3
-                &:nth-child(4)
-                    grid-area: 4 / 2 / 6 / 3
-                &:nth-child(5)
-                    grid-area: 1 / 3 / 3 / 4
-                &:nth-child(6)
-                    grid-area: 3 / 3 / 6 / 4
-        &.nine
-            grid-template-columns: repeat(12, 1fr)
-            grid-template-rows: repeat(7, 1fr)
-            & .mounting__item
-                &:nth-child(1)
-                    grid-area: 1 / 1 / 3 / 5
-                &:nth-child(2)
-                    grid-area: 3 / 1 / 6 / 5
-                &:nth-child(3)
-                    grid-area: 1 / 5 / 4 / 9
-                &:nth-child(4)
-                    grid-area: 4 / 5 / 6 / 9
-                &:nth-child(5)
-                    grid-area: 1 / 9 / 3 / 13
-                &:nth-child(6)
-                    grid-area: 3 / 9 / 6 / 13
-                &:nth-child(7)
-                    grid-area: 6 / 1 / 8 / 4
-                &:nth-child(8)
-                    grid-area: 6 / 4 / 8 / 7
-                &:nth-child(9)
-                    grid-area: 6 / 7 / 8 / 13
+                    flex-basis: 26%
     &__item
         padding: 24px
         width: 100%
-        height: 100%
+        height: auto
+        &:nth-child(1)
+            flex-basis: 30%
+        &:nth-child(2)
+            flex-basis: 30%
+        &:nth-child(3)
+            flex-basis: 40%
     &__img
         width: 100%
         height: 100%
@@ -127,6 +90,7 @@ export default {
         filter: grayscale(1)
         transition: filter .3s ease
         & img
+            border-radius: 8px
             width: 100%
             height: 100%
         &:hover
